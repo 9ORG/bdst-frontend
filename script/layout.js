bc846255f658aab0a82ef78152fce92d90055081
summerz0501 <summerz0501@gmail.com>
2014-02-02 00:30:32 +0800

    +club/club-id001 +activity/activity-id1024

--- script/layout.js
+++ script/layout.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 
 	var w = $("#username").width();
-	$("#top-user-menu").mouseenter(function(){
-		// $("#top-logo-text").css("color","#fff");
+	$("#username").mouseenter(function(){
 		$("#top-user-menu").css("width",220+w);
 		$("#popup-user-menu").removeClass("animated fadeOutLeft");
 		$("#popup-user-menu").addClass("animated fadeInLeft");
 		$("#popup-user-menu").removeClass("hidden");
 	});
 	$("#top-user-menu").mouseleave(function(){
-		// $("#top-logo-text").css("color","#ff9900");
 		$("#popup-user-menu").removeClass("animated fadeInLeft");
 		$("#popup-user-menu").addClass("animated fadeOutLeft",function(){
 			$("#popup-user-menu").addClass("hidden");
@@ -50,6 +48,94 @@ $(document).ready(function(){
 		$("#popup-user-list-custom").addClass("animated fadeOutLeft",function(){
 			$("#popup-user-list-custom").addClass("hidden");
 		});
+	});
+
+	/* 活动首页 - Tab切换 */
+	$("#activity-detail-tab-button").click(function(){
+		$(this).addClass("current");
+		$("#activity-library-tab-button").removeClass("current");
+		$("#activity-detail-tab").removeClass("hidden");
+		$("#activity-library-tab").addClass("hidden");
+	});
+	$("#activity-library-tab-button").click(function(){
+		$(this).addClass("current");
+		$("#activity-detail-tab-button").removeClass("current");
+		$("#activity-library-tab").removeClass("hidden");
+		$("#activity-detail-tab").addClass("hidden");
+	});
+
+	/* 社团首页 - 全屏背景 */
+	var theWindow = $(window),
+		$cover = $("#club-cover"),
+		aspectRatio = $cover.width() / $cover.height();
+	function resizeCover() {
+		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+			$cover
+				.removeClass()
+				.addClass("cover-height")
+				.css("top",$(window).height()/2 - $cover.height()/2);
+		} else {
+			$cover
+				.removeClass()
+				.addClass("cover-width")
+				.css("top",$(window).height()/2 - $cover.height()/2);
+		}
+	}
+	theWindow.resize(resizeCover).trigger("resize");
+	/* 社团首页 - 封面图集 */
+	$(".club-cover-img1").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover1.jpg");
+		$(".club-cover-img1").addClass("current");
+		$(".club-cover-img2").removeClass("current");
+		$(".club-cover-img3").removeClass("current");
+		$(".club-cover-img4").removeClass("current");
+		$(".club-cover-img5").removeClass("current");
+		$(".club-cover-img6").removeClass("current");
+	});
+	$(".club-cover-img2").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover2.jpg");
+		$(".club-cover-img1").removeClass("current");
+		$(".club-cover-img2").addClass("current");
+		$(".club-cover-img3").removeClass("current");
+		$(".club-cover-img4").removeClass("current");
+		$(".club-cover-img5").removeClass("current");
+		$(".club-cover-img6").removeClass("current");
+	});
+	$(".club-cover-img3").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover3.jpg");
+		$(".club-cover-img1").removeClass("current");
+		$(".club-cover-img2").removeClass("current");
+		$(".club-cover-img3").addClass("current");
+		$(".club-cover-img4").removeClass("current");
+		$(".club-cover-img5").removeClass("current");
+		$(".club-cover-img6").removeClass("current");
+	});
+	$(".club-cover-img4").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover4.jpg");
+		$(".club-cover-img1").removeClass("current");
+		$(".club-cover-img2").removeClass("current");
+		$(".club-cover-img3").removeClass("current");
+		$(".club-cover-img4").addClass("current");
+		$(".club-cover-img5").removeClass("current");
+		$(".club-cover-img6").removeClass("current");
+	});
+	$(".club-cover-img5").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover5.jpg");
+		$(".club-cover-img1").removeClass("current");
+		$(".club-cover-img2").removeClass("current");
+		$(".club-cover-img3").removeClass("current");
+		$(".club-cover-img4").removeClass("current");
+		$(".club-cover-img5").addClass("current");
+		$(".club-cover-img6").removeClass("current");
+	});
+	$(".club-cover-img6").click(function(){
+		$("#club-cover").attr("src","../img/club-id001/club-id001-cover6.jpg");
+		$(".club-cover-img1").removeClass("current");
+		$(".club-cover-img2").removeClass("current");
+		$(".club-cover-img3").removeClass("current");
+		$(".club-cover-img4").removeClass("current");
+		$(".club-cover-img5").removeClass("current");
+		$(".club-cover-img6").addClass("current");
 	});
 
 });
